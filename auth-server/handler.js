3f5a17100615a84fc2b577e5f743b3caa42d4d9f
Daniela Codes <daniela.antonio.430@gmail.com>
2024-05-30 13:04:39 -0400

    Writing Functins
    In handler.js :
    Adding AuthURL Function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,15 +1,40 @@
 'use strict';
 
-exports.hello = async (event) => {
+const {
+  google
+} = require("googleapis");
+const calendar = google.calendar("v3");
+const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
+const {
+  CLIENT_SECRET,
+  CLIENT_ID,
+  CALENDAR_ID
+} = process.env;
+const redirect_uris = [
+  "https://yungflare.github.io/meet/"
+];
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  redirect_uris[0]
+);
+
+module.exports.getAuthURL = async () => {
+
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v2.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      authUrl,
+    }),
   };
 };
