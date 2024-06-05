5a730a4b5b61982451cb61228e19a39cd181feea
Daniela Codes <daniela.antonio.430@gmail.com>
2024-06-05 19:05:09 -0400

    Created Api.js file
    Imported mockData & created functions

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -27,4 +27,21 @@ describe('<CitySearch /> component', () => {
         expect(suggestionList).toHaveClass('suggestions');
     });
 
+    test('updates list of suggestions correctly when user types in city textbox', async () => {
+        const user = userEvent.setup();
+        const allEvents = await getEvents();
+        const allLocations = extractLocations(allEvents);
+        CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.type(cityTextBox, "Berlin");
+        const suggestions = allLocations? allLocations.filter((location) => {
+        return location.toUpperCase().indexOf(cityTextBox.value.toUpperCase()) > -1;
+        }): [];
+        const suggestionListItems = CitySearchComponent.queryAllByRole('listitem');
+        expect(suggestionListItems).toHaveLength(suggestions.length + 1);
+        for (let i = 0; i < suggestions.length; i += 1) {
+        expect(suggestionListItems[i].textContent).toBe(suggestions[i]);
+    }
+  });
+    
 });
