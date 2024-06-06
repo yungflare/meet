c89192c88b02bd4835869d88a25b1f9b3d47a210
Daniela Codes <daniela.antonio.430@gmail.com>
2024-06-06 17:02:00 -0400

    End of Scenario 2

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CitySearch from "../components/CitySearch";
+import { extractLocations, getEvents } from "../api";
 
 describe('<CitySearch /> component', () => {
     let CitySearchComponent;
--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,16 +1,40 @@
 import { useState } from "react";
 
-const CitySearch = () => {
-    const [showSuggestions, setShowSuggestions] = useState(false);
+const CitySearch = ({ allLocations }) => {
+    const [ showSuggestions, setShowSuggestions ] = useState(false);
+    const [ query, setQuery ] = useState("");
+    const [ suggestions, setSuggestions ] = useState([]);
+
+    const handleInputChanged = (event) => {
+        const value = event.target.value;
+        const filteredLocations = allLocations ? allLocations.filter((location) => {
+            return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+        }) : [];
+        setQuery(value);
+        setSuggestions(filteredLocations);
+       };
+
     return (
         <div id="city-search">
             <input 
             type="text"
             className="city"
             placeholder="Search for a city"
+            value={query}
             onFocus={() => setShowSuggestions(true)}
+            onChange={handleInputChanged}
             />
-            {showSuggestions ? <ul className="suggestions"></ul> : null}
+            {showSuggestions ? 
+            <ul className="suggestions">
+                {suggestions.map((suggestion) => {
+                    return <li key={suggestion}>{suggestion}</li>
+                })}
+                <li key='See all cities'>
+                    <b>See all cities</b>
+                </li>
+            </ul> 
+            : null
+            }
         </div>
     )
 }
